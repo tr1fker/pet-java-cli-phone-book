fde1b928be4eaf4ddd0037b27391ae68486964a9
tr1fker <cfjhxhkxck@gmail.com>
2025-08-23 22:44:08 +0300

    Added method to output custom menu

---
+++ src/main/java/tr1fker/view/OutputConsole.java
@@ -0,0 +1,15 @@
+package tr1fker.view;
+
+public class OutputConsole {
+    public void printCustomMenu(){
+        System.out.println("""
+Пользовательское меню
+1. Добавить контакт
+2. Просмотреть список контактов
+3. Найти контакт
+4. редактировать контакт
+5. Удалить контакт
+6. Выйти
+                """);
+    }
+}
