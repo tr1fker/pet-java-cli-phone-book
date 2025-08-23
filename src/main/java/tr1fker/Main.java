98793a6a3bbed480ccf7ea6985b95150f9d16f6c
tr1fker <cfjhxhkxck@gmail.com>
2025-08-23 22:44:25 +0300

    Test method of output custom menu

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,7 +1,10 @@
 package tr1fker;
 
+import tr1fker.view.OutputConsole;
+
 public class Main {
     public static void main(String[] args) {
-        
+        OutputConsole outputConsole = new OutputConsole();
+        outputConsole.printCustomMenu();
     }
 }
