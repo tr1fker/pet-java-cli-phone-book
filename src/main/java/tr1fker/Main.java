194b0d010ea2ed7bd1400ef1d06040e812d0d6d4
tr1fker <cfjhxhkxck@gmail.com>
2025-08-24 22:28:27 +0300

    Included Recordhandler class

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,13 +1,15 @@
 package tr1fker;
 
 import tr1fker.handlers.InputHandler;
+import tr1fker.handlers.RecordHandler;
 import tr1fker.handlers.UIHandler;
 
 public class Main {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
+        RecordHandler recordHandler = new RecordHandler();
 
-        UIHandler uIHandler = new UIHandler(inputHandler);
+        UIHandler uIHandler = new UIHandler(inputHandler, recordHandler);
         uIHandler.startConsole();
     }
 }
