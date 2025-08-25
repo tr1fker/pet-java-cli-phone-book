99af9a51aa0aac8597d72184666dc9f7164f35a8
tr1fker <cfjhxhkxck@gmail.com>
2025-08-25 23:34:39 +0300

    Realized loading and saveing records

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,15 +1,22 @@
 package tr1fker;
 
+import tr1fker.handlers.GsonHandler;
 import tr1fker.handlers.InputHandler;
 import tr1fker.handlers.RecordHandler;
 import tr1fker.handlers.UIHandler;
 
 public class Main {
+    private static final String filePathRecords = "records.json";
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
-        RecordHandler recordHandler = new RecordHandler();
+        GsonHandler gsonHandler = new GsonHandler(filePathRecords);
+        RecordHandler recordHandler = new RecordHandler(gsonHandler);
 
         UIHandler uIHandler = new UIHandler(inputHandler, recordHandler);
+        recordHandler.loadRecords();
+
         uIHandler.startConsole();
+
+        recordHandler.saveRecords();
     }
 }
